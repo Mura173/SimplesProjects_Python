71e3911a932803f99f177fac14737c4b8b19ebfc
Murilo Ângelo <murapb07@gmail.com>
2024-08-16 10:41:45 -0300

    Exercicios Simples
    - Usuários cadastrar algumas informações
    
    - Comparação de qual número é maior
    
    - IMC Simples
    
    - Usuários cadastrar algumas informações com F Strings

---
+++ Credenciais.py
@@ -0,0 +1,13 @@
+nome = 'Murilo'
+sobrenome = 'Braggio'
+idade = 17
+ano_nasc = 2024 - idade
+maior_idade = idade >= 18
+altura_m = 1.68
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nasc)
+print('É maior de idade? ', maior_idade)
+print('Altura em metros: ', altura_m)
---
+++ IMC.py
@@ -0,0 +1,10 @@
+nome = 'Murilo'
+altura = 1.68
+peso = 77
+imc = peso / altura ** 2
+
+print (nome, 'tem', altura, 'de altura')
+print(peso, 'quilos e seu IMC é:', imc)
+
+print(...)
+# IMC = peso/(altura x altura)
